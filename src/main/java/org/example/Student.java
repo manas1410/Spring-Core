38ac38e518a37df6157aed550e8dc49b835ddc65
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-10-05 17:28:09 +0530

    Add files via upload

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String studentAddress;
+
+    public Student(int studentId, String studentName, String studentAddress) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+    }
+
+    public Student() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                ", studentAddress='" + studentAddress + '\'' +
+                '}';
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
